a534a5dd2e20774151d09d374198302fe62cb07f
Naseef Mashood <nosfeeferatu@gmail.com>
2022-12-05 12:02:21 +0530

    Added Click Event Listener

---
+++ app.js
@@ -0,0 +1,10 @@
+var inputText = document.querySelector(".input-text");
+var btn = document.querySelector("button");
+var outputDiv = document.querySelector("#output");
+
+
+btn.addEventListener("click", eventHandler);
+
+function eventHandler(){
+    console.log(inputText);
+}
