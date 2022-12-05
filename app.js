ac24121e8cfea25c296a699ccb377a27020c5e50
Naseef Mashood <nosfeeferatu@gmail.com>
2022-12-05 12:04:35 +0530

    fetch and wire fetch

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var inputText = document.querySelector(".input-text");
 var btn = document.querySelector("button");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function fetchingServerURL(text){
+    return serverURL + "?" + "text="  + text;
+}
 
 btn.addEventListener("click", eventHandler);
 
 function eventHandler(){
-    console.log(inputText.value);
     outputDiv.innerText = inputText.value;
+    fetch(fetchingServerURL(inputText.value))
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
